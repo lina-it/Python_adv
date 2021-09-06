a62781e31aa792e61524762e022bf20d03f25641
lina-it <linashka@ukr.net>
2021-09-06 09:12:43 +0300

    HW4

--- HW4_Adv_Lina.py
+++ HW4_Adv_Lina.py
@@ -1,24 +1,26 @@
 ##Не совсем поняла нужно ли было в другом формате забрасывать сюда код или вот так:
 ##Если что, пиши. Спасибо!)
 
-# CREATE TABLE orders(
-# 	order_id integer PRIMARY KEY AUTOINCREMENT NOT NULL,
-# 	created_dt date,
-# 	updated_dt date,
-# 	order_type text,
-# 	description text,
-# 	status text,
-# 	serial_no integer,
-# 	creator_id integer);
-#
-# CREATE TABLE employees(
-# 	employee_id integer PRIMARY KEY AUTOINCREMENT NOT NULL,
-# 	fio text,
-# 	position_ text,
-# 	department_id integer,
-# 	foreign key(department_id) references departments(department_id)
-# 	);
-#
-# CREATE TABLE departments(
-# 	department_id integer PRIMARY KEY AUTOINCREMENT NOT NULL,
-# 	department_name text);
+"""CREATE TABLE if not exists departments(
+	department_id serial PRIMARY KEY NOT NULL,
+	department_name text);
+
+CREATE TABLE if not exists employees(
+	employee_id serial PRIMARY KEY NOT NULL,
+	fio text,
+	position text,
+	department_id integer,
+	foreign key(department_id) references departments(department_id)
+	);
+
+CREATE TABLE if not exists orders(
+	order_id serial PRIMARY KEY NOT NULL,
+	created_dt date,
+	updated_dt date,
+	order_type text,
+	description text,
+	status text,
+	serial_no integer,
+	creator_id integer,
+	foreign key(creator_id) references employees(employee_id)
+	);"""
